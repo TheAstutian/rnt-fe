b33d3bb9fd329903ef168904ae88b25e440cc15b
TheAstutian <speakingatoms@gmail.com>
2024-01-27 08:40:19 +0000

    fixing bugs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Logo from '../img/logo.png';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <footer>
       <img src={Logo} alt="" />
-      <span> Made with love and <b>React.js</b>.</span>
+      <span> Made with love and <Link to ="/login"><b>React.js</b>.</Link></span>
     </footer>
   )
 }
